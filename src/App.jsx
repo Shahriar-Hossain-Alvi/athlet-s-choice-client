51529ac28de67735c9a997c11fa90508f97af7b0
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-08-14 23:24:48 +0600

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-lg text-red-600'>Hello world</h1>
+
+        <button className="btn btn-lg btn-primary">Click me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is landing page</h1>
+        </div>
+    );
+};
+
+export default Home;

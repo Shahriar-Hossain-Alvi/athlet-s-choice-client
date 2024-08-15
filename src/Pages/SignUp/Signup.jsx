78764141d0709bf18bef65f49052a9fd81716f41
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-08-15 21:02:15 +0600

    added functio to upload profile picture to imgBB and get the url

--- src/Pages/SignUp/Signup.jsx
+++ src/Pages/SignUp/Signup.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import { AuthContext } from "../../Providers/AuthProvider";
+import useAxiosPublic from "../../components/Hooks/useAxiosPublic";
+import axios from "axios";
 
 const image_hosting_api = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_api}`;
@@ -8,20 +10,22 @@ const image_hosting_url = `https://api.imgbb.com/1/upload?key=${image_hosting_ap
 
 const Signup = () => {
     const { loading, user, setLoading, createUser, } = useContext(AuthContext);
+    const axiosPublic = useAxiosPublic();
 
-    const handleSignup = e => {
+
+
+
+    const handleSignup = async e => {
         e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
 
         if (user) {
             toast.error('Already Signed in');
             form.reset();
         }
-
-        const form = e.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        const image = form.image.files[0];
 
         if (password.length < 6) {
             toast.warning('Password should contain at least 6 characters');
@@ -35,9 +39,20 @@ const Signup = () => {
         }
 
         //upload image
+        const imageFile = {image: form.image.files[0]}
+        const res = await axios.post(image_hosting_url, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+        const imgURL = res.data.data.display_url;
 
-        console.log(email, password);
+        const userInfo = {
+            name, email, password, imgURL
+        }
 
+        console.log(userInfo);
+        
     }
 
     return (
